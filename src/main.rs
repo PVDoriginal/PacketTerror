92870995bbcd8571b51bf1ebb2076a3d2fee2a5d
PescaMA <pescariu.mate@gmail.com>
2025-04-22 17:30:40 +0300

    add copyable shop items

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use bevy::prelude::*;
 
-use bevy_inspector_egui::bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use camera::CameraPlugin;
 use game::GamePlugin;
 use shop::ShopPlugin;
 
-pub mod shop;
 pub mod camera;
 pub mod game;
+pub mod grid;
+pub mod shop;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(EguiPlugin)
-        .add_plugins((GamePlugin, CameraPlugin,ShopPlugin))
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins((GamePlugin, CameraPlugin, ShopPlugin))
         .run();
 }
--- src/shop/mod.rs
+++ src/shop/mod.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use shop_items::init_shop_items;
+
+pub mod shop_items;
 
 pub struct ShopPlugin;
 
@@ -13,13 +16,17 @@ pub struct UpdateCurrencyEvent(i32);
 impl Plugin for ShopPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Currency { value: 0 });
+        app.add_systems(Startup, init_shop_items);
         app.add_systems(Update, update_currency);
         app.add_event::<UpdateCurrencyEvent>();
     }
 }
 
-pub fn update_currency(mut currency: ResMut<Currency>, mut event_remove : EventReader<UpdateCurrencyEvent>) {
-    for ev in  event_remove.read(){
+pub fn update_currency(
+    mut currency: ResMut<Currency>,
+    mut event_remove: EventReader<UpdateCurrencyEvent>,
+) {
+    for ev in event_remove.read() {
         currency.value += ev.0;
     }
 }
