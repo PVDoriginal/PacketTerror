738af1f5aa1b0f3eeda4e4023a072d59236bfc21
PescaMA <pescariu.mate@gmail.com>
2025-04-22 17:47:56 +0300

    unfinished code

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,1 +1,7 @@
+use bevy::prelude::*;
 
+pub const GRID_SIZE: usize = 30;
+#[derive(Default, Resource)]
+pub struct Grid {
+    grid: [[Option<Entity>; GRID_SIZE]; GRID_SIZE],
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use camera::CameraPlugin;
 use game::GamePlugin;
+use grid::Grid;
 use shop::ShopPlugin;
 
 pub mod camera;
@@ -12,6 +13,7 @@ pub mod shop;
 
 fn main() {
     App::new()
+    .init_resource::<Grid>()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(WorldInspectorPlugin::new())
         .add_plugins((GamePlugin, CameraPlugin, ShopPlugin))
