bb4aeeb8345c2258ae36f32d7faef6fcc5f026a9
PescaMA <pescariu.mate@gmail.com>
2025-04-24 22:42:30 +0300

    cable check

--- src/grid/interaction.rs
+++ src/grid/interaction.rs
@@ -2,7 +2,11 @@ use bevy::{math::vec3, prelude::*};
 
 use crate::{
     camera::{SCALE, SPRITE_SIZE},
-    shop::{currency::{Currency, UpdateCurrencyEvent}, shop_items::ShopItem},
+    items::{PC, Router, Switch},
+    shop::{
+        currency::{Currency, UpdateCurrencyEvent},
+        shop_items::{ItemType, ShopItem},
+    },
 };
 
 use super::Grid;
@@ -15,9 +19,19 @@ impl Plugin for InteractionPlugin {
     }
 }
 
-pub fn make_interactable(mut commands: Commands, shop_items: Query<Entity, Added<ShopItem>>) {
-    for item in &shop_items {
-        commands.entity(item).observe(drag_item).observe(drop_item);
+pub fn make_interactable(
+    mut commands: Commands,
+    shop_items: Query<(Entity, &ShopItem), Added<ShopItem>>,
+) {
+    for (item, item_type) in &shop_items {
+        match item_type.item_type {
+            ItemType::Cable => {
+                commands.entity(item).observe(drag_item).observe(drop_cable);
+            }
+            _ => {
+                commands.entity(item).observe(drag_item).observe(drop_item);
+            }
+        }
     }
 }
 
@@ -45,25 +59,70 @@ pub fn drop_item(
         return;
     };
     transform.translation.z = 0.;
+    if can_place_item(&transform, &shop_item, &grid, &currency)
+        && grid.on_empty_cell(transform.translation.truncate())
+    {
+        let pos = grid
+            .world_to_grid(transform.translation.truncate())
+            .unwrap();
 
-    if currency.value < shop_item.price as i32 {
-        transform.translation = shop_item.pos.extend(0.); // snap back
-        return;
+        let mut obj = commands.spawn((
+            sprite.clone(),
+            Transform::from_translation(pos.extend(0).as_vec3() * SPRITE_SIZE),
+        ));
+        shop_item.item_type.add_component(&mut obj);
+
+        grid.grid[pos.x as usize][pos.y as usize] = Some(obj.id());
+        writer.send(UpdateCurrencyEvent(-1 * shop_item.price as i32));
     }
 
-    let Some(pos) = grid.world_to_grid(transform.translation.truncate()) else {
-        transform.translation = shop_item.pos.extend(0.); // snap back
+    // snap back:
+    transform.translation = shop_item.pos.extend(0.);
+}
+
+fn can_place_item(
+    transform: &Mut<Transform>,
+    shop_item: &ShopItem,
+    grid: &ResMut<Grid>,
+    currency: &Res<Currency>,
+) -> bool {
+    if currency.value < shop_item.price as i32 {
+        return false;
+    }
+    grid.inside_grid(transform.translation.truncate())
+}
+
+pub fn drop_cable(
+    trigger: Trigger<Pointer<DragEnd>>,
+    mut transforms: Query<(&mut Transform, &ShopItem)>,
+    mut grid_vals: Query<Entity, Or<(With<PC>, With<Switch>, With<Router>)>>,
+    grid: ResMut<Grid>,
+    currency: Res<Currency>,
+) {
+    let Ok((mut transform, shop_item)) = transforms.get_mut(trigger.entity()) else {
         return;
     };
+    transform.translation.z = 0.;
+    if can_place_item(&transform, &shop_item, &grid, &currency)
+        && cable_can_connect(&transform, &grid, &mut grid_vals)
+    {
+        info!("yep!");
+    }
 
-    let mut obj = commands.spawn((
-        sprite.clone(),
-        Transform::from_translation(pos.extend(0).as_vec3() * SPRITE_SIZE),
-    ));
-    grid.grid[pos.x as usize][pos.y as usize] = Some(obj.id());
-    shop_item.item_type.add_component(&mut obj);
-
+    // snap back:
     transform.translation = shop_item.pos.extend(0.);
+}
 
-    writer.send(UpdateCurrencyEvent(-1 * shop_item.price as i32));
+fn cable_can_connect(
+    transform: &Mut<Transform>,
+    grid: &ResMut<Grid>,
+    grid_vals: &mut Query<Entity, Or<(With<PC>, With<Switch>, With<Router>)>>,
+) -> bool {
+    let Some(entity) = grid.get_element(transform.translation.truncate()) else {
+        return false;
+    };
+    let Ok(_) = grid_vals.get_mut(entity) else {
+        return false;
+    };
+    true
 }
--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -16,7 +16,8 @@ pub struct Grid {
 }
 
 impl Grid {
-    fn inside_grid(&self, pos: Vec2) -> bool {
+    fn inside_grid(&self, mut pos: Vec2) -> bool {
+        pos += SPRITE_SIZE / 2.;
         if pos.x >= GRID_N as f32 * SPRITE_SIZE || pos.x < 0. {
             return false;
         }
@@ -26,16 +27,24 @@ impl Grid {
         return true;
     }
     pub fn world_to_grid(&self, mut pos: Vec2) -> Option<UVec2> {
-        pos += 10.5;
         if !self.inside_grid(pos) {
             return None;
         }
-
-        let pos = uvec2((pos.x / SPRITE_SIZE) as u32, (pos.y / SPRITE_SIZE) as u32);
-        if self.grid[pos.x as usize][pos.y as usize].is_none() {
-            return Some(pos);
-        }
-        None
+        pos += SPRITE_SIZE / 2.;
+        Some(uvec2(
+            (pos.x / SPRITE_SIZE) as u32,
+            (pos.y / SPRITE_SIZE) as u32,
+        ))
+    }
+    pub fn on_empty_cell(&self, pos: Vec2) -> bool {
+        let Some(pos) = self.world_to_grid(pos) else {
+            return false;
+        };
+        self.grid[pos.x as usize][pos.y as usize].is_none()
+    }
+    pub fn get_element(&self, pos: Vec2) -> Option<Entity> {
+        let pos = self.world_to_grid(pos)?;
+        self.grid[pos.x as usize][pos.y as usize]
     }
 }
 
@@ -44,7 +53,11 @@ pub fn init_grid(mut commands: Commands, asset_server: Res<AssetServer>) {
         for j in 0..GRID_M {
             commands.spawn((
                 Sprite::from_image(asset_server.load("grid_cell.png")),
-                Transform::from_translation(vec3(i as f32 * SPRITE_SIZE, j as f32 * SPRITE_SIZE, -1.)),
+                Transform::from_translation(vec3(
+                    i as f32 * SPRITE_SIZE,
+                    j as f32 * SPRITE_SIZE,
+                    -1.,
+                )),
                 Name::new("Grid_block"),
             ));
         }
