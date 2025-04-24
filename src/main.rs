e4a6354dca63a8f517459c593e1be2a56a8aff89
Damon <pvdoriginal2019@gmail.com>
2025-04-24 20:49:28 +0300

    Revert "added fixed resolution"
    This reverts commit 687bc97c52e4d76c59859e0d58a4023851907595.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy::window::{CursorOptions, WindowResolution};
 
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use camera::CameraPlugin;
@@ -15,28 +14,9 @@ pub mod items;
 pub mod main_menu;
 pub mod shop;
 
-const WINDOW_WIDTH: f32 = 1280.0;
-const WINDOW_HEIGHT: f32 = 720.0;
-
-fn window_setup() -> WindowPlugin {
-    WindowPlugin {
-        primary_window: Some(Window {
-            title: "Packet Terror".to_string(),
-            resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-            resizable: false,
-            ..default()
-        }),
-        ..default()
-    }
-}
-
 fn main() {
     App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(window_setup())
-                .set(ImagePlugin::default_nearest()),
-        )
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(WorldInspectorPlugin::new())
         .add_plugins((
             CameraPlugin,
