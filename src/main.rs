048cbbf7fff9c1bc3a83bfe0354097006d0725fa
mihaid-asm <mihai.duzi@gmail.com>
2025-04-25 17:23:23 +0300

    added health
    need to test it

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use camera::CameraPlugin;
 use game::GamePlugin;
 use grid::GridPlugin;
+use health::HealthPlugin;
 use items::ItemsPlugin;
 use main_menu::MainMenuPlugin;
 use shop::ShopPlugin;
@@ -11,6 +12,7 @@ use shop::ShopPlugin;
 pub mod camera;
 pub mod game;
 pub mod grid;
+pub mod health;
 pub mod items;
 pub mod main_menu;
 pub mod shop;
@@ -26,6 +28,7 @@ fn main() {
             ShopPlugin,
             ItemsPlugin,
             MainMenuPlugin,
+            HealthPlugin,
         ))
         .run();
 }
