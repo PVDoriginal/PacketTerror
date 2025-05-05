149ff4639be3f1abc139c99075271ddf0670c12d
PescaMA <pescariu.mate@gmail.com>
2025-05-05 11:11:27 +0300

    make packets deal damage to PC

--- src/items/mod.rs
+++ src/items/mod.rs
@@ -2,32 +2,29 @@ use bevy::prelude::*;
 use cables::CablesPlugin;
 use enemy_pc::EnemyPCPlugin;
 use packets::PacketsPlugin;
+use pc::PcsPlugin;
 use projectiles::ProjectilePlugin;
 use routers::RoutersPlugin;
 use servers::ServersPlugin;
 use switches::SwitchesPlugin;
 use upgrades::UpgradesPlugin;
 
-use crate::game::InGame;
-
 pub mod cables;
 pub mod enemy_pc;
 pub mod packets;
+pub mod pc;
 pub mod projectiles;
 pub mod routers;
 pub mod servers;
 pub mod switches;
 pub mod upgrades;
 
-#[derive(Component)]
-#[require(InGame)]
-pub struct PC;
-
 pub struct ItemsPlugin;
 
 impl Plugin for ItemsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
+            PcsPlugin,
             PacketsPlugin,
             CablesPlugin,
             EnemyPCPlugin,
