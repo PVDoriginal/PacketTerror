0dc7312d61c762057abac84f89ce46c47a7ad0af
PescaMA <pescariu.mate@gmail.com>
2025-06-09 18:43:39 +0300

    make hit last 1s

--- src/items/packets.rs
+++ src/items/packets.rs
@@ -1,6 +1,13 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 
 use crate::{camera::ScreenShake, game::InGame};
+
+#[derive(Component)]
+struct HitTime {
+    timer: Timer,
+}
 
 #[derive(Component, Clone)]
 #[require(InGame)]
@@ -80,14 +87,13 @@ pub struct PacketsPlugin;
 impl Plugin for PacketsPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<PacketDamageEvent>();
-        app.add_systems(Update, receive_damage);
+        app.add_systems(Update, (receive_damage, packet_fx, packet_end_hit).chain());
     }
 }
 
 fn receive_damage(
     mut event: EventReader<PacketDamageEvent>,
     mut packets: Query<&mut Packet>,
-    mut shake: ResMut<ScreenShake>,
     mut commands: Commands,
 ) {
     for e in event.read() {
@@ -98,7 +104,50 @@ fn receive_damage(
         packet.hp -= e.damage;
         if packet.hp <= 0 {
             commands.entity(e.target).try_despawn();
-            shake.shake(1., 0.1);
+        }
+    }
+}
+
+fn packet_fx(
+    mut event: EventReader<PacketDamageEvent>,
+    packets: Query<&Packet>,
+    mut sprites: Query<&mut Sprite>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+
+    mut screen_shake: ResMut<ScreenShake>,
+) {
+    for e in event.read() {
+        let Ok(packet) = packets.get(e.target) else {
+            continue;
+        };
+
+        let Ok(mut sprite) = sprites.get_mut(e.target) else {
+            continue;
+        };
+
+        if packet.hp <= 0 {
+            screen_shake.shake(1., 0.1);
+        } else {
+            *sprite = Sprite::from_image(asset_server.load("white_packet.png"));
+
+            commands.entity(e.target).insert(HitTime {
+                timer: Timer::new(Duration::from_secs(1), TimerMode::Once),
+            });
+        }
+    }
+}
+
+fn packet_end_hit(
+    mut packets: Query<(&mut Sprite, &mut HitTime), With<Packet>>,
+    time: Res<Time>,
+    asset_server: Res<AssetServer>,
+) {
+    for (mut sprite, mut hit_time) in packets.iter_mut() {
+        hit_time.timer.tick(time.delta());
+
+        if hit_time.timer.finished() {
+            *sprite = Sprite::from_image(asset_server.load("enemy_packet.png"));
         }
     }
 }
